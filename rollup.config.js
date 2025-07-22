7a3c3cdff44880841c79a92a1878ede38910a0bd
Hecris10 <helamaewerton@gmail.com>
2025-07-22 11:13:58 -0600

    refactor: switch from ES module to CommonJS syntax in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import { readFileSync } from 'fs';
-import dts from 'rollup-plugin-dts';
+const commonjs = require('@rollup/plugin-commonjs');
+const resolve = require('@rollup/plugin-node-resolve');
+const typescript = require('@rollup/plugin-typescript');
+const { readFileSync } = require('fs');
+const { dts } = require('rollup-plugin-dts');
 
 const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
 
-export default [
+module.exports = [
   // Main build
   {
     input: 'src/index.ts',
